2967a44b6a315b1139a309438005cbecdf205c14
roboticcc <cosmeanq@protonmail.com>
2022-07-07 12:06:31 +0600

    added migrations with test data & balance model

--- database/migrations/2022_07_06_154706_create_certificates_table.php
+++ database/migrations/2022_07_06_154706_create_certificates_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Certificate;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Carbon;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -28,6 +29,33 @@ return new class extends Migration
 
             $table->softDeletes();
         });
+
+        Certificate::insert([
+            [
+                'buyer_name' => 'Ivan',
+                'buyer_surname' => 'Ivanov',
+                'buyer_email' => 'ivan@ivan.com',
+                'tree' => 'India 2021',
+                'amount' => '3',
+                'cost' => '117',
+                'status' => 1,
+                'activation_key' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'buyer_name' => 'Nikita',
+                'buyer_surname' => 'Petrov',
+                'buyer_email' => 'petrov@nikita.com',
+                'tree' => 'Bali 2015',
+                'amount' => '4',
+                'cost' => '156',
+                'status' => 1,
+                'activation_key' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ]);
     }
 
     /**
